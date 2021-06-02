09b3f534159d8683c40956acb931934843409b6b
Fox <foxtam7@ya.ru>
2021-06-02 14:56:33 +0300

    Add InterruptBotException

---
+++ src/main/java/net/foxtam/InterruptBotException.java
@@ -0,0 +1,7 @@
+package net.foxtam;
+
+public class InterruptBotException extends RuntimeException {
+    public InterruptBotException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
