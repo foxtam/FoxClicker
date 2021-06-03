dcf23e47796bdfc9a598fc135851e169211ae4e6
Fox <foxtam7@ya.ru>
2021-06-03 14:01:24 +0300

    Add 3 ways to load image

--- FoxClicker/src/main/java/net/foxtam/Image.java
+++ FoxClicker/src/main/java/net/foxtam/Image.java
@@ -1,26 +1,73 @@
 package net.foxtam;
 
 import net.foxtam.exceptions.BufferedImageProcessingException;
+import net.foxtam.exceptions.LoadImageException;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBuffer;
 import java.awt.image.DataBufferByte;
 import java.awt.image.DataBufferInt;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.ref.Cleaner;
+
 
 public class Image {
     private static final Cleaner cleaner = Cleaner.create();
+
     static {
         nu.pattern.OpenCV.loadShared();
     }
 
     private final Mat matrix;
 
-    public Image(BufferedImage bufferedImage) {
-        this.matrix = bufferedImgToMat(bufferedImage);
+    private Image(Mat matrix) {
+        this.matrix = matrix;
         cleaner.register(this, matrix::release);
+    }
+
+    public static Image loadFromBufferedImage(BufferedImage bufferedImage) {
+        return new Image(bufferedImgToMat(bufferedImage));
+    }
+
+    public static Image loadFromFile(String path) {
+        try {
+            return tryLoadFromFile(path);
+        } catch (IOException e) {
+            throw new LoadImageException("Failed to load image: " + path, e);
+        }
+    }
+
+    private static Image tryLoadFromFile(String path) throws IOException {
+        BufferedImage image = ImageIO.read(new FileInputStream(path));
+        Mat matrix = bufferedImgToMat(image);
+        Imgproc.cvtColor(matrix, matrix, Imgproc.COLOR_BGR2GRAY);
+        return new Image(matrix);
+    }
+
+    public static Image loadFromResource(String path) {
+        try {
+            return tryLoadFromResource(path);
+        } catch (IOException e) {
+            throw new LoadImageException("Failed to load image: " + path, e);
+        }
+    }
+
+    private static Image tryLoadFromResource(String path) throws IOException {
+        try (InputStream resourceAsStream = Image.class.getResourceAsStream(path)) {
+            if (resourceAsStream == null) {
+                throw new IOException();
+            }
+            BufferedImage image = ImageIO.read(resourceAsStream);
+            Mat matrix = bufferedImgToMat(image);
+            Imgproc.cvtColor(matrix, matrix, Imgproc.COLOR_BGR2GRAY);
+            return new Image(matrix);
+        }
     }
 
     private static Mat bufferedImgToMat(BufferedImage bufferedImage) {
