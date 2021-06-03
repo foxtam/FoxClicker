04217cd8914a148dc2027db124bd7b5d82d85e5c
Fox <foxtam7@ya.ru>
2021-06-03 13:59:52 +0300

    Add class LoadImageException

---
+++ FoxClicker/src/main/java/net/foxtam/exceptions/LoadImageException.java
@@ -0,0 +1,7 @@
+package net.foxtam.exceptions;
+
+public class LoadImageException extends FoxClickerException {
+    public LoadImageException(String msg, Throwable e) {
+        super(msg, e);
+    }
+}
