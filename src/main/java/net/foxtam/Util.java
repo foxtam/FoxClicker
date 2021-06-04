7de83709cf9f095280cbaacc5f911fd74609d058
Fox <foxtam7@ya.ru>
2021-06-04 12:25:00 +0300

    Add Util class

---
+++ src/main/java/net/foxtam/Util.java
@@ -0,0 +1,11 @@
+package net.foxtam;
+
+public class Util {
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
