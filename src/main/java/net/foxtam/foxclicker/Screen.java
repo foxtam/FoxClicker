43d8e365a143f86be874388d2d8bc01e67281a9e
Fox <foxtam7@ya.ru>
2021-06-05 10:52:45 +0300

    Complete getCapture

--- src/main/java/net/foxtam/foxclicker/Screen.java
+++ src/main/java/net/foxtam/foxclicker/Screen.java
@@ -1,12 +1,19 @@
 package net.foxtam.foxclicker;
 
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Screen {
     
     public Image getCapture(Rectangle rectangle) {
-        BufferedImage screenCapture = BotRobot.INSTANCE.createScreenCapture(rectangle);
-        
+        BufferedImage screenCapture = Robo.INSTANCE.createScreenCapture(rectangle);
+        Mat matScreenCapture = Image.getMatFrom(screenCapture);
+        Mat grayScreen = new Mat();
+        Imgproc.cvtColor(matScreenCapture, grayScreen, Imgproc.COLOR_BGR2GRAY);
+        matScreenCapture.release();
+        return Image.fromMat(grayScreen);
     }
 }
