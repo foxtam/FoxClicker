fa63f9c8c515db8ebb60ee4d03cc00d66684f834
Fox <foxtam7@ya.ru>
2021-06-26 21:16:08 +0300

    Update deleteRecursively

--- src/main/java/net/foxtam/foxclicker/GlobalLogger.java
+++ src/main/java/net/foxtam/foxclicker/GlobalLogger.java
@@ -1,11 +1,12 @@
 package net.foxtam.foxclicker;
 
+import lombok.Cleanup;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -83,29 +84,25 @@ public class GlobalLogger {
         getCurrentLogger().trace(getIndent() + "< {}", actualClassMethodName);
     }
 
+    @SneakyThrows
     public static void deleteRecursively(Path directory) {
-        try (Stream<Path> walk = Files.walk(directory)) {
-            //noinspection ResultOfMethodCallIgnored
-            walk.sorted(Comparator.reverseOrder())
-                  .map(Path::toFile)
-                  .forEach(File::delete);
-        } catch (IOException e) {
-//            Bot.showErrorMessage("Can't delete: " + directory);
-//            throw new RuntimeException(e);
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            e.printStackTrace(new PrintWriter(outputStream, true, StandardCharsets.UTF_8));
-            trace(outputStream.toString(StandardCharsets.UTF_8));
-        }
-    }
-
-    public static <T> T trace(T obj) {
-        getCurrentLogger().trace(INDENT + getIndent() + obj.toString());
-        return obj;
+        @Cleanup
+        Stream<Path> walk = Files.walk(directory);
+        //noinspection ResultOfMethodCallIgnored
+        walk.sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
     }
 
     public static void trace(Exception e) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         e.printStackTrace(new PrintWriter(outputStream, true, StandardCharsets.UTF_8));
         trace(outputStream.toString(StandardCharsets.UTF_8));
+    }
+
+    @SuppressWarnings("UnusedReturnValue")
+    public static <T> T trace(T obj) {
+        getCurrentLogger().trace(INDENT + getIndent() + obj.toString());
+        return obj;
     }
 }
